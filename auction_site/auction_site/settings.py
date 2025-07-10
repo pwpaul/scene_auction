1858b4458add9a38142a60350e4d5e20e5e0aafa
Paul Beauvais <paul.beauvais@gmail.com>
2025-07-10 11:56:36 -0400

    set static

--- auction_site/auction_site/settings.py
+++ auction_site/auction_site/settings.py
@@ -16,6 +16,10 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+STATIC_ROOT = BASE_DIR / 'static'
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
